c6968285e6a8edae4abf7bfecf3d2e4efd75b127
ElAlbany <128701669+ElAlbany@users.noreply.github.com>
2024-04-15 22:48:05 +0200

    Add files via upload

---
+++ News.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <map>
+#include< vector>
+#include "UserRepository.h"
+using namespace std;
+class News
+{
+	string title;
+	string description;
+	string date;
+	string category;
+	float rate;  // this is the actual rate comes from summing rates and divied on their size
+	map<string, int > allRate; // username and his rate // rates can be edited so we need the username ,so map is convenient for that
+
+public:
+
+	static vector<News> news; // main data structure to store all news
+
+
+};
+
