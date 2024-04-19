774d6c6c86889a6a698ea8410b3b3dd171649b89
ElAlbany <128701669+ElAlbany@users.noreply.github.com>
2024-04-19 19:18:41 +0200

    Update User.h

--- News_Mangement_Sys/User.h
+++ News_Mangement_Sys/User.h
@@ -3,7 +3,8 @@
 #include <iostream>
 #include <string>
 #include <vector>
-
+#include<unordered_set>
+#include<unordered_map>
 
 
 using namespace std;
@@ -14,6 +15,7 @@ private:
 public:
     static vector<User> users;
     static string currentUsername, currentPassword;
+    static unordered_map<string, unordered_set<string>> bookmarks;
 
     User(string username, string password);
 
@@ -94,5 +96,84 @@ public:
         cout << "rate : " << News::news[num - 1].getRate() << "\n";
     }
 
+    static void bookmarkingMenu() {
+        cout << "[1] add article to your favourite \n";
+        cout << "[2] delete marked article \n";
+        cout << "[3] display all marked articles \n";
+
+        cout << "press -1 to skip\n";
+        cout << "\n";
+        cout << "enter your choice : ";
+        int choice;
+        cin >> choice;
+        if (choice == 1) {
+            AddToBookmarks();
+        } else if (choice == 2) {
+            RemoveFromBookmarks();
+        } else if (choice == 3) {
+            PrintBookmarks();
+        } else if (choice == -1)
+            return;
+        else {
+            cout << "please select one of the shown operations above \n";
+            bookmarkingMenu();
+        }
+
+
+    }
+
+    static void AddToBookmarks() {
+        int num;
+        News::displayAllNews();
+        cout << "enter number : ";
+        cin >> num;
+        if (num < 1 || num > News::news.size()) {
+            cout << "you have entered wrong number \n";
+            return;
+        }
+        bool flag = User::IsInBookmarks(News::news[num - 1].getTitle());
+        if (flag) {
+            cout << "already exists\n";
+            return;
+        }
+        User::bookmarks[User::currentUsername].insert(News::news[num - 1].getTitle());
+        cout << "added successfully\n";
+    }
+
+    static bool IsInBookmarks(string title) {
+        return (User::bookmarks[User::currentUsername].find(title) != User::bookmarks[User::currentUsername].end());
+    }
+
+    static void RemoveFromBookmarks() {
+        User::PrintBookmarks();
+        int num;
+        cout << "enter number to remove : ";
+        cin >> num;
+        if (num < 1 || num > User::bookmarks[User::currentUsername].size()) {
+            cout << "you have entered wrong one \n";
+            return;
+        }
+        auto it = User::bookmarks[User::currentUsername].begin();
+        std::advance(it, num - 1);
+        User::bookmarks[User::currentUsername].erase(it);
+        cout << "removed successfully\n";
+
+    }
+
+    static void PrintBookmarks() {
+        auto &it = User::bookmarks[User::currentUsername];
+        if (it.empty())
+            return void(cout << "You don't have bookmarks.\n");
+
+        cout << "Your Bookmarks :\n";
+        int i = 1;
+        for (auto &it2: it) {
+            cout << "[" << i++ << "]" << it2 << "\n";
+            cout << "~~~~~~";
+        }
+        cout << "~~~~~~~~~~~~~~~~~~~~\n";
+    }
+//khaled
+////////////////////////////////////////////////////////////
 };
 
